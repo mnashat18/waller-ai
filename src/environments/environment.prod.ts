680c081b9ba3a2789557831060560735a2b082ed
mnashat18 <mnashat1806@gmail.com>
2026-02-09 15:01:30 +0200

    Update API_URL in production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'https://dash.conntinuity.com',
+  API_URL: 'https://conntinuity.com',
   ADMIN_TOKEN_ENDPOINT: '',
   ADMIN_TOKEN_SECRET: '',
   AUDIT_LOG_REVIEW_ROLE_IDS: [] as string[]
