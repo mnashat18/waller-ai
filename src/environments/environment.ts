bb108a50b803cb26ecf0f8b30b290b148dcbd0c9
mnashat18 <mnashat1806@gmail.com>
2026-02-09 15:01:50 +0200

    Update API_URL to new endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  API_URL: 'https://dash.conntinuity.com',
+  API_URL: 'https://conntinuity.com',
   ADMIN_TOKEN_ENDPOINT: 'http://127.0.0.1:3001/admin-token',
   ADMIN_TOKEN_SECRET: '',
   AUDIT_LOG_REVIEW_ROLE_IDS: [] as string[]
