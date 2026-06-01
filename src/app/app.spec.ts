1d08a792d47252d260de925e5afc976c80f4e350
mnashat18 <mnashat1806@gmail.com>
2026-06-01 09:39:44 +0300

    Update app tests

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, wellar-ui');
+    expect(compiled.querySelector('.root-route-shell')).toBeTruthy();
   });
 });
