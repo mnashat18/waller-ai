02b382db87acb7b3d07aa35ca08988544f0826ba
mnashat18 <mnashat1806@gmail.com>
2026-06-01 09:39:59 +0300

    Update Angular bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+declare const ngDevMode: boolean | undefined;
+
+if (typeof ngDevMode === 'undefined' || ngDevMode) {
+  await import('@angular/compiler');
+}
+
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
