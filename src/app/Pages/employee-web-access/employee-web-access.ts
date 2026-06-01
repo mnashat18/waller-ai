9ef71f5061cdf216d7a4daf746244ebeb1f8fad2
mnashat18 <mnashat1806@gmail.com>
2026-06-01 11:16:15 +0300

    Add employee web access page module

---
+++ src/app/Pages/employee-web-access/employee-web-access.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-web-access-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './employee-web-access.html',
+  styleUrl: './employee-web-access.css'
+})
+export class EmployeeWebAccessPageComponent {}
